add6ade0be6fc4709cf67117a53c1e78cd9207eb
Kavika <kbpalletenne@gmail.com>
2024-07-25 13:30:29 +1000

    initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
 
 export default function Document() {
   return (
